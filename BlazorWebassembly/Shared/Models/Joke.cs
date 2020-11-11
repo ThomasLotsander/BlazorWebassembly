88ecfec7902e299aa24886669ff77e2a851ffe24
thomas lotsander <thomaslotsander@gmail.com>
2020-11-11 21:01:13 +0100

    initial

---
+++ BlazorWebassembly/Shared/Models/Joke.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorWebassembly.Shared.Models
+{
+    public class Joke
+    {
+        public int ID { get; set; }
+
+        public string Premiss { get; set; }
+
+        public string PunchLine { get; set; }
+
+    }
+}
